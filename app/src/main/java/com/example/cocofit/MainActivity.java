d5f57e3a992dee762839b8e482542a9317871979
Kevin Burgos <s.burgoshh@gmail.com>
2021-02-27 13:29:07 +0100

    barra superior de notificaciones oculta

--- app/src/main/java/com/example/cocofit/MainActivity.java
+++ app/src/main/java/com/example/cocofit/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         btInicio = findViewById(R.id.btInicio);
