97977bd58373c9ef9d5d4802457f86bcb6660474
Kevin Burgos <s.burgoshh@gmail.com>
2021-02-27 13:17:17 +0100

    Pantalla de inicio funcionando

---
+++ app/src/main/java/com/example/cocofit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cocofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button btInicio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btInicio = findViewById(R.id.btInicio);
+        btInicio.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btInicio) {
+            Toast.makeText(this,"Siguiente ventana",Toast.LENGTH_LONG).show();
+        }
+    }
+}
