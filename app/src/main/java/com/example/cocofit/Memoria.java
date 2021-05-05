e1bb597a294d2ae98e487a4b92d2deed91aba8f2
Kevin Burgos <s.burgoshh@gmail.com>
2021-05-05 21:22:00 +0200

    empiezo aquí en mayo

--- app/src/main/java/com/example/cocofit/Memoria.java
+++ app/src/main/java/com/example/cocofit/Memoria.java
@@ -2,13 +2,35 @@ package com.example.cocofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Memoria extends AppCompatActivity {
+public class Memoria extends AppCompatActivity implements OnClickListener {
+    private AnimationDrawable miAnimacion;
+    private ImageView cara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memoria);
+
+        cara = new ImageView(this);
+        cara.setOnClickListener(this);
+        cara.setBackgroundResource(R.drawable.carta1);
+        miAnimacion = (AnimationDrawable) cara.getBackground();
+        miAnimacion.start();
+        setContentView(cara);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(cara)) {
+            Toast.makeText(this,"click imagen",Toast.LENGTH_LONG).show();
+            miAnimacion.start();
+        }
     }
 }
